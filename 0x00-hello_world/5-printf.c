24ce97415867e13bb6f0f40e9e7f0aa7d7c4c1fc
Okokobi Joel <kpermoghena@gmail.com>
2022-10-13 18:56:16 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: returns zero after program running
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a peice of art,\n");
+}
