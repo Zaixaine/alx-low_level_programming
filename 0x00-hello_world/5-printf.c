1b5cbd7e309313a63b52a69c62f0ad44f5096d8b
Okokobi Joel <kpermoghena@gmail.com>
2022-10-13 19:15:02 -0700

    Corrections for Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point of the program
- *
- * Return: returns zero after program running
+ *  * main-entry point for the program.
+ *   *
+ *    * Return:0 if no errors, return non zero if errors
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a peice of art,\n");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
