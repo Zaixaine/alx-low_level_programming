e74cddc2282410ae86f1a469f5f54100e66d3b37
Okokobi Joel <kpermoghena@gmail.com>
2022-10-14 20:20:28 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - program start point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char i;
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			if (i == 'e' || i == 'q')
+				continue;
+			putchar(i);
+		}
+			putchar(i);
+			return (0);
+}
