a91c17df86d06fb6c42e7d86871ba58decfe6303
Okokobi Joel <kpermoghena@gmail.com>
2022-10-18 21:57:55 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,12 @@
  */
 int print_last_digit(int n)
 {
-	int x;
+	int ld = n % 10;
 
-	if (n < 0)
-		n = -n;
-	x = n % 10;
-	_putchar (x + '0');
+	if (ld < 0)
+		ld = -1;
 
-	return (x);
+	_putchar (ld + '0');
+
+	return (0);
 }
