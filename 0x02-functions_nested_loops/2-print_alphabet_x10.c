2ecc754d228bbce6c2bbec73c44155ced78c672c
Okokobi Joel <kpermoghena@gmail.com>
2022-10-18 19:54:58 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Printing the alphabet 10 times in lowercase
+ * Return: return 0
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char la;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (la = 'a'; la <= 'z'; la++)
+			_putchar(la);
+		_putchar('\n');
+	}
