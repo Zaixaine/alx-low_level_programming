205dfb8195e2769efdfe12fc00d5c5c92b5aae45
Okokobi Joel <kpermoghena@gmail.com>
2022-10-20 14:24:17 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ * Return: 0 or 1
+ */
+int _putchar(char c)
+{
+	return (write(i, &c, 1));
+}
